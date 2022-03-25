2207b395aa43aad0d8bf4e1dc243cf3327d3b897
bestinwoo <bestinwoo@gmail.com>
2022-03-25 17:20:37 +0900

    first commit

---
+++ src/main/java/project/inhaAuction/InhaAuctionApplication.java
@@ -0,0 +1,13 @@
+package project.inhaAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhaAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhaAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/inhaAuction/InhaAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package project.inhaAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhaAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
