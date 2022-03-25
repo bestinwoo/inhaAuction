4cebe766a9c7c29366ff7b91bacb52b2a53856de
bestinwoo <bestinwoo@gmail.com>
2022-03-26 04:36:17 +0900

    Spring Security 설정, MvcConfig 설정

---
+++ src/main/java/project/inhaAuction/SecurityConfig.java
@@ -0,0 +1,26 @@
+package project.inhaAuction;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/**").permitAll(); // TODO: 나중에 authenticated로 바꾸기
+        
+    }
+}
