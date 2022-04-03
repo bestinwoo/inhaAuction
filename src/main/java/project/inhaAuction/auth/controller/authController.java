f6d875810ca9c3d30c25057b2c0ed7750c2d249a
bestinwoo <bestinwoo@gmail.com>
2022-04-04 00:55:11 +0900

    Feat: JWT 적용

--- src/main/java/project/inhaAuction/auth/controller/authController.java
+++ src/main/java/project/inhaAuction/auth/controller/authController.java
@@ -1,17 +1,10 @@
 package project.inhaAuction.auth.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class authController {
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public String register() {
-        return "auth/register";
-    }
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String login() {
-        return "auth/login";
-    }
+
 }
